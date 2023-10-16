86f489aa82a791f664648f6bb9c36d6ca280f6cb
GE3ka <hela_jbeli@hotmail.com>
2023-10-16 20:27:57 +0300

    first js commt

---
+++ JS/Day1/Core/ButtonClicker/ButtonClicker.js
@@ -0,0 +1,34 @@
+let logIn = document.getElementById("loginBtn");
+
+function log(){
+    if(logIn.innerText ==="Login"){
+        logIn.innerText = "Logout";
+    }
+    else{
+        logIn.innerText = "Login";
+    }
+
+}
+
+let disappearBtn = document.getElementById("definition");
+function disappear(){
+
+    disappearBtn.style.visibility="hidden";
+    
+}
+
+let nbNinja = document.getElementById("nbNinjaLikes");
+function ninjaLikes(){
+    nb=nbNinja.innerText;   
+    nb++;
+    nbNinja.innerText =nb;    
+}
+
+
+let nbProg = document.getElementById("nbProgLikes");
+function progLikes(){
+    nb=nbProg.innerText;
+    nb++;
+    nbProg  .innerText =nb;    
+}
+
